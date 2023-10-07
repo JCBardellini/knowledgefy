c9f49d935c7601be049c5564e3a87a773b4b38f3
JCBardellini <115193170+JCBardellini@users.noreply.github.com>
2023-10-07 14:37:22 -0700

    audio books display done, working on search and home page

--- src/pages/AudioBooks/index.jsx
+++ src/pages/AudioBooks/index.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { fetchAudioBookItems } from "../../Redux/APICalls/audiobooks";
+import PlayCircleFilledRounded from "@mui/icons-material/PlayCircleFilledRounded";
+import "./index.css";
 
 const Library = () => {
   const [audioBook, setAudioBook] = useState([]);
@@ -14,7 +16,46 @@ const Library = () => {
     fetchData();
   }, []);
   console.log("audioBook", audioBook);
-  return <div>Library</div>;
+  // follow what i did for the podcast to display the content
+  // need to exclude index of 0, there is no content there
+  const excludeArray = [0];
+  // now map through the data to get the title of each category, but need to filter out the exclude array
+  const audioBooksContainer = audioBook.map((item) => {
+    if (
+      !excludeArray.includes(item.index) &&
+      item.content &&
+      item.content.items.length > 0
+    ) {
+      return (
+        <div key={item.id}>
+          <h3 className="subtitle">{item.name}</h3>
+          <div className="contentContainer">
+            {item.content.items.map((content) => (
+              <div key={content.id} className="eachContainer">
+                <div>
+                  <img
+                    src={content.images[0]?.url}
+                    alt={content.name}
+                    className="audioBookCover"
+                  />
+                </div>
+                <h4 className="eachAudioBookTitle">{content.name}</h4>
+                {content.publisher && (
+                  <p className="publisher">{content.publisher.split(",")[0]}</p>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    }
+  });
+  return (
+    <div className="audioBooks">
+      <h1 className="mainTitle">AudioBooks</h1>
+      {audioBooksContainer}
+    </div>
+  );
 };
 
 export default Library;

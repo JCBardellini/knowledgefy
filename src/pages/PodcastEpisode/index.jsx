9d12e89dc5cc831cb1e0584d3b60f045ad99e3da
JCBardellini <115193170+JCBardellini@users.noreply.github.com>
2023-10-10 02:02:19 -0700

    got the content for podcast

--- src/pages/PodcastEpisode/index.jsx
+++ src/pages/PodcastEpisode/index.jsx
@@ -1,31 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { fetchPodcastItems } from "../../Redux/APICalls/podcastApiCall";
+import { useParams } from "react-router-dom";
 
 const PodcastEpisode = () => {
-  const [podcastInfo, setPodcastInfo] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchPodcastItems();
-      setPodcastInfo(data);
-    };
+  // checking the local storage for this data
+  const storedContent = JSON.parse(localStorage.getItem("podcastData"));
+  // using the hook useParams, to get the information from the param
+  const { id } = useParams();
+  console.log(id);
+  let item = null;
+  for (const contentItem of storedContent) {
+    for (const episode of contentItem.content.items) {
+      if (episode.id === id) {
+        item = episode;
+        break;
+      }
+    }
+    if (item) {
+      break;
+    }
+  }
+  console.log(item);
 
-    fetchData();
-  }, []);
-  console.log("podcastinfo", podcastInfo);
-
-  const podcastEpisode = podcastInfo.map((item) => {
-    return (
-      <div>
-        {item.name}
-        <div>
-          {item.content.items.map((content) => {
-            return <h4>{content.name}</h4>;
-          })}
-        </div>
-      </div>
-    );
-  });
-  return <div>{podcastEpisode}</div>;
+  return (
+    <div>
+      {item.name}
+      {item.description}
+      <img src={item.images[0]?.url} alt="" />
+    </div>
+  );
 };
 
 export default PodcastEpisode;
